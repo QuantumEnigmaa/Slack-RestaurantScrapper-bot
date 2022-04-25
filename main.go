b1988b714e6361293dbf1ed1c2cbd2ad0129b790
Vaisseau Thibaut <T0254584@gemalto.com>
2022-04-25 15:43:44 +0200

    making progress but hard to scrap website due to poor quality

--- main.go
+++ main.go
@@ -2,17 +2,53 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/gocolly/colly"
 )
 
+type dish struct {
+	dishName    string
+	description string
+	price       int
+}
+
 func main() {
-	collector := colly.NewCollector(colly.AllowedDomains("boutique.wysifood.fr"))
+	scrapper("boutique.wysifood.fr", "https://boutique.wysifood.fr/5bde4fa022f9e550534f93537e604529/take-away/menu/15809")
+}
 
-	collector.OnHTML(".item-head", func(e *colly.HTMLElement) {
-		fmt.Printf("Printing whole element : %v\n", e.Text)
+func scrapper(domain string, address string) {
+	collector := colly.NewCollector(colly.AllowedDomains(domain))
 
+	collector.OnHTML("div[id=menu-content]", func(e *colly.HTMLElement) {
+		/* e.ForEach(".row", func(_ int, el *colly.HTMLElement) {
+			fmt.Printf("Dish name : %v\n", e.ChildText("div.col-md-6 > h4.item-head"))
+		}) */
+
+		dishName := strings.Split(e.ChildText("div.row > div.col-md-6 > h4.item-head"), "\n")
+		for _, name := range dishName {
+			strings.Fields(name)
+			fmt.Printf("Dish Name : %v\n", strings.Fields(name))
+		}
+
+		fmt.Println("#####################")
+		fmt.Println("DISH DESCRIPTION")
+
+		description := strings.Split(e.ChildText("div.row > div.col-md-6 > div.item-description"), "\n")
+		for _, desc := range description {
+			strings.Fields(desc)
+			fmt.Printf("Dish description : %v\n", strings.Fields(desc))
+		}
+
+		fmt.Println("#####################")
+		fmt.Println("DISH PRICE")
+
+		prices := strings.Split(e.ChildText("div.row > div.item-cart > span.item-cart-price"), "\n")
+		for _, price := range prices {
+			strings.Fields(price)
+			fmt.Printf("Price : %v\n", strings.Fields(price))
+		}
 	})
 
-	collector.Visit("https://boutique.wysifood.fr/5bde4fa022f9e550534f93537e604529/take-away/menu")
+	collector.Visit(address)
 }
